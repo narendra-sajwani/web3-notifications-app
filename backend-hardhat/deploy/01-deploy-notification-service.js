4b37b8dcb56d169b9530d855bdea6cae775d64af
narendra-sajwani <niks.imagination@gmail.com>
2022-11-20 20:18:24 +0530

    completed hardhat backend and setup basic graph and frontend

---
+++ backend-hardhat/deploy/01-deploy-notification-service.js
@@ -0,0 +1,25 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("--------------------------")
+    const args = []
+    const notificationService = await deploy("NotificationService", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(notificationService.address, args)
+    }
+    log("--------------------------")
+}
+
+module.exports.tags = ["all", "notificationService"]
--- backend-hardhat/hardhat.config.js
+++ backend-hardhat/hardhat.config.js
@@ -1,6 +1,64 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const REPORT_GAS = process.env.REPORT_GAS
+
 module.exports = {
-  solidity: "0.8.17",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+            saveDeployments: true,
+            gasPrice: 130000000000,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+        ],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+}
